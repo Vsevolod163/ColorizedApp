4b50de79a95d575dc1e1e0dfbc50f867259de294
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-24 03:18:57 +0300

    Change color of view function added

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private var colorView: UIView!
+    @IBOutlet private var redColorLabel: UILabel!
+    @IBOutlet private var greenColorLabel: UILabel!
+    @IBOutlet private var blueColorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction private func changeColorOfView(_ sender: UISlider) {
+        let valueOfSlider = (round(sender.value * 100) / 100)
+  
+        if sender.minimumTrackTintColor == .systemPink {
+            redColorLabel.text = valueOfSlider.formatted()
+            colorView.backgroundColor = .systemRed
+            colorView.alpha = CGFloat(valueOfSlider)
+        } else if sender.minimumTrackTintColor == .systemGreen {
+            greenColorLabel.text = valueOfSlider.formatted()
+            colorView.backgroundColor = .systemGreen
+            colorView.alpha = CGFloat(valueOfSlider)
+        } else if sender.minimumTrackTintColor == .systemBlue {
+            blueColorLabel.text = valueOfSlider.formatted()
+            colorView.backgroundColor = .systemBlue
+            colorView.alpha = CGFloat(valueOfSlider)
+        }
+    }
 }
 
