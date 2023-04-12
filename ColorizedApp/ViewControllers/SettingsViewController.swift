ab3da2d75e69f1bc79ea611dab7807e003e89cfc
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-12 23:12:11 +0300

    Top methods added

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -15,13 +15,9 @@ final class SettingsViewController: UIViewController {
     @IBOutlet private var greenColorLabel: UILabel!
     @IBOutlet private var blueColorLabel: UILabel!
     
-    @IBOutlet private var redSlider: UISlider!
-    @IBOutlet private var greenSlider: UISlider!
-    @IBOutlet private var blueSlider: UISlider!
+    @IBOutlet private var sliders: [UISlider]!
     
-    @IBOutlet var redSliderTF: UITextField!
-    @IBOutlet var greenSliderTF: UITextField!
-    @IBOutlet var blueSliderTF: UITextField!
+    @IBOutlet var slidersTextFields: [UITextField]!
     
     var viewColor: UIColor!
     
@@ -30,43 +26,51 @@ final class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        redSliderTF.delegate = self
-        greenSliderTF.delegate = self
-        blueSliderTF.delegate = self
+        addDoneToKeyboard(textFields: slidersTextFields)
+        
+        for textField in slidersTextFields {
+            textField.delegate = self
+        }
         
         colorView.layer.cornerRadius = 10
         
         colorView.backgroundColor = viewColor
         
-        redSlider.value = Float(getRGBFromView().red)
-        greenSlider.value = Float(getRGBFromView().green)
-        blueSlider.value = Float(getRGBFromView().blue)
+        for (slider, color) in zip(sliders, getRGBFromView()) {
+            slider.value = Float(color)
+        }
         
         setColor()
         setValuesToColorLabels()
         setValuesToTF()
     }
     
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
-        setColor()
-        setValuesToColorLabels()
+        for textField in slidersTextFields {
+            guard let newValue = textField.text else { return }
+            guard let numberValue = Float(newValue), (0...1).contains(numberValue), newValue.count <= 4 else {
+                showAlert(textField: textField)
+                return
+            }
+            
+            checkTagsOfTextFields(textField: textField, numberValue: numberValue)
+        }
     }
     
     @IBAction private func changeColorOfView(_ sender: UISlider) {
         setColor()
-        switch sender {
-        case redSlider:
-            redColorLabel.text = string(from: redSlider)
-            redSliderTF.text = string(from: redSlider)
-        case greenSlider:
-            greenColorLabel.text = string(from: greenSlider)
-            greenSliderTF.text = string(from: greenSlider)
+        switch sender.tag {
+        case 0:
+            redColorLabel.text = string(from: sliders[0])
+            slidersTextFields[0].text = string(from: sliders[0])
+        case 1:
+            greenColorLabel.text = string(from: sliders[1])
+            slidersTextFields[1].text = string(from: sliders[1])
         default:
-            blueColorLabel.text = string(from: blueSlider)
-            blueSliderTF.text = string(from: blueSlider)
+            blueColorLabel.text = string(from: sliders[2])
+            slidersTextFields[2].text = string(from: sliders[2])
         }
     }
     
@@ -78,7 +82,7 @@ final class SettingsViewController: UIViewController {
         dismiss(animated: true)
     }
     
-    private func getRGBFromView() -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+    private func getRGBFromView() -> [CGFloat] {
         var red: CGFloat = 0.0
         var green: CGFloat = 0.0
         var blue: CGFloat = 0.0
@@ -86,30 +90,30 @@ final class SettingsViewController: UIViewController {
         
         viewColor?.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
         
-        return (red, green, blue, alpha)
+        return [red, green, blue]
     }
     
     private func setColor() {
         colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
+            red: CGFloat(sliders[0].value),
+            green: CGFloat(sliders[1].value),
+            blue: CGFloat(sliders[2].value),
             alpha: 1
         )
     }
     
     private func setValuesToColorLabels() {
-        redColorLabel.text = string(from: redSlider)
-        greenColorLabel.text = string(from: greenSlider)
-        blueColorLabel.text = string(from: blueSlider)
+        redColorLabel.text = string(from: sliders[0])
+        greenColorLabel.text = string(from: sliders[1])
+        blueColorLabel.text = string(from: sliders[2])
     }
     
     private func setValuesToTF() {
-        redSliderTF.text = string(from: redSlider)
-        greenSliderTF.text = string(from: greenSlider)
-        blueSliderTF.text = string(from: blueSlider)
+        for (textField, slider) in zip(slidersTextFields, sliders) {
+            textField.text = string(from: slider)
+        }
     }
-    
+
     private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
@@ -119,27 +123,66 @@ final class SettingsViewController: UIViewController {
 extension SettingsViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         guard let newValue = textField.text else { return }
-        guard let numberValue = Float(newValue) else { return }
+        guard let numberValue = Float(newValue), (0...1).contains(numberValue), newValue.count <= 4 else {
+            showAlert(textField: textField)
+            return
+        }
         
+        checkTagsOfTextFields(textField: textField, numberValue: numberValue)
+    }
+    
+    @objc private func addActionToDoneButton() {
+        for textField in slidersTextFields {
+            guard let newValue = textField.text else { return }
+            guard let numberValue = Float(newValue), (0...1).contains(numberValue), newValue.count <= 4 else {
+                showAlert(textField: textField)
+                return
+            }
+            
+            checkTagsOfTextFields(textField: textField, numberValue: numberValue)
+            
+            textField.resignFirstResponder()
+        }
+    }
+    
+    private func checkTagsOfTextFields(textField: UITextField, numberValue: Float) {
         if textField.tag == 0 {
-            redSlider.setValue(numberValue, animated: true)
+            sliders[0].setValue(numberValue, animated: true)
         } else if textField.tag == 1 {
-            greenSlider.setValue(numberValue, animated: true)
+            sliders[1].setValue(numberValue, animated: true)
         } else if textField.tag == 2 {
-            blueSlider.setValue(numberValue, animated: true)
+            sliders[2].setValue(numberValue, animated: true)
         }
         
         setColor()
         setValuesToColorLabels()
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        for sliderTextField in [redSliderTF, greenSliderTF, blueSliderTF] {
-            if textField == sliderTextField {
-                textFieldDidEndEditing(textField)
-            }
-        }
+    private func addDoneToKeyboard(textFields: [UITextField]) {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
         
-        return true
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(addActionToDoneButton))
+        toolbar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), doneButton]
+        
+        for textField in textFields {
+            textField.inputAccessoryView = toolbar
+        }
+    }
+    
+    private func showAlert(textField: UITextField) {
+        let alert = UIAlertController(
+            title: "Invalid format",
+            message: "Please, enter value from 0 to 1",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField.text = ""
+            textField.becomeFirstResponder()
+        }
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
     }
 }
